d432ad1ed9242966b17417f2517be536a6befda2
Denis Zdorovtsov <mrtrizer@gmail.com>
2024-02-17 21:00:18 +0900

    Added DefaultEditorWindow

---
+++ Editor/DefaultEditorWindow.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEditor;
+
+namespace SiegeUp.Core.Editor
+{
+    public class DefaultEditorWindow : EditorWindow
+    {
+        public Action<EditorWindow> onGUI;
+        public Action onClosed;
+        protected virtual void OnGUI() => onGUI?.Invoke(this);
+        void OnInspectorUpdate() => Repaint();
+        void OnDestroy() => onClosed?.Invoke();
+    }
+}
